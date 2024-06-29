caff6f9afcbe48df16fbdc0a386c2afd84f9d68b
AnginJitu <jituangin@gmail.com>
2024-06-29 08:47:33 +0700

    create sub domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://anginjitu.asia',
+      url: 'https://5.anginjitu.asia',
       lastModified: new Date(),
     },
   ]
